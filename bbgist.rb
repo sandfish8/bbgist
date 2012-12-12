c2a8cf2e14ea7b5e94c5eda52f2632bf421e306e
Sam Cooper <sam@chgworks.com>
2012-12-12 09:50:25 -0800

    authentication seems to be working

--- bbgist.rb
+++ bbgist.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'netaddr'
+require 'cgi'
 
 class Array
   def del_in_place(value)
@@ -11,13 +13,81 @@ class Bbgist < Sinatra::Base
 
   SERVER = "http://localhost:4567"
   UPLOAD_DIR = "uploads"
+  ID_LENGTH = 10
+
+  # referenced from https://thevault.blueboxgrid.com/index.php/IP_Space_Allocation
+  TRUSTED_NETWORKS = ["208.85.144.0/21",
+                      "67.214.208.0/20",
+                      "199.91.168.0/22",
+                      "199.182.120.0/22"]
+
+  USERNAME = "bbgist"
+  PASSWORD = "rumor1:newts"
+
+  # before do
+  #   authenticate
+  # end
+
+  configure do
+    enable :sessions
+    set :session_secret, "big_secret"
+  end
 
   # show all files
   get '/' do
-    entries = Dir.entries(path(UPLOAD_DIR)).del_in_place(".").del_in_place("..")
-    entries_with_mtime = get_mtimes(entries)
-    entries_with_mtime.sort! { |a,b| b.last <=> a.last }
-    entries_with_mtime.map { |e| "#{e.first}\t#{e.last.strftime('%Y-%m-%d %H:%M:%S')}" }.join("\n") + "\n"
+    if !session["authorized"] && !trusted_ip?
+      status, headers, body = call env.merge("PATH_INFO" => '/login')
+      [status, headers, body]
+    else
+      content_type :text
+      entries = Dir.entries(path(UPLOAD_DIR)).del_in_place(".").del_in_place("..")
+      entries_with_mtime = get_mtimes(entries)
+      entries_with_mtime.sort! { |a,b| b.last <=> a.last }
+      entries_with_mtime.map { |e| "#{e.first}\t#{e.last.strftime('%Y-%m-%d %H:%M:%S')}" }.join("\n") + "\n"
+    end
+  end
+
+  # def authenticate
+  #   #if !trusted_ip
+  #   if true
+  #     erb :login
+  #   end
+  # end
+
+  get '/login' do
+    @login_failed = params.key?("login_failed")
+    erb :login
+  end
+
+  get '/logout' do
+    session["authorized"] = false
+    redirect '/'
+  end
+
+  post '/login' do
+    request.body.rewind
+    data = CGI.parse(request.body.read)
+    username = data["username"].first
+    password = data["password"].first
+
+    if username != USERNAME || password != PASSWORD
+      redirect '/login?login_failed', 303 # see comment below
+      # status, headers, body = call env.merge("PATH_INFO" => '/login')
+      # [status, headers, body]
+    else
+      # start session send them to /
+      session["authorized"] = true
+      redirect '/', 303 # http://www.gittr.com/index.php/archive/details-of-sinatras-redirect-helper/
+    end
+  end
+
+  def trusted_ip?
+    TRUSTED_NETWORKS.each do |n|
+      if NetAddr::CIDR.create(n).contains?(request.ip)
+        return true
+      end
+    end
+    false
   end
 
   def get_mtimes(entries)
@@ -28,6 +98,7 @@ class Bbgist < Sinatra::Base
 
   # show a file
   get '/:name' do |name|
+    content_type :text
     return usage if name == "help"
     begin
       File.open(path(UPLOAD_DIR, name), 'r') do |file|
@@ -41,7 +112,7 @@ class Bbgist < Sinatra::Base
   # recieve a text upload
   post "/" do
 
-    name = rand_alpha_chars(4)
+    name = rand_alpha_chars(ID_LENGTH)
 
     File.open(path(UPLOAD_DIR, name), "w") do |f|
       f.write(params[:file][:tempfile].read)
@@ -55,11 +126,11 @@ class Bbgist < Sinatra::Base
   end
 
   def rand_alpha_chars(num)
-    (0...num).map{  65.+(rand(26)).chr}.join
+    (0...num).map{  97.+(rand(26)).chr}.join
   end
 
   def usage
-    "curl -F file=@FILE_NAME http://#{SERVER}\n"
+    "curl -F file=@FILE_NAME #{SERVER}\n"
   end
 
 end
--- config.ru
+++ config.ru
@@ -1,3 +1,2 @@
-#require 'bbgist'
 require File.join(File.dirname(__FILE__), 'bbgist')
 run Bbgist.new
